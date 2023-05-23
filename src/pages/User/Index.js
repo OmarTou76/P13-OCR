5f41f19ce8a91166caf5444110e3cb785814e24f
omar <omar.tourabi7@gmail.com>
2023-05-24 00:23:26 +0200

    feat: UserPage / set user name

--- src/pages/User/Index.js
+++ src/pages/User/Index.js
@@ -1,52 +1,61 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import "./user.css"
 import { transactions } from '../../data/transactions'
 import { BankAccount } from '../../components/BankAccount/Index'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router'
+import { setEditingState } from '../../redux/user/editUser'
+import { userEditor } from '../../redux/user/actions'
 
 export const User = () => {
     const { data, isLogged } = useSelector(state => state.user)
+    const { isEditingUser, error } = useSelector(state => state.userEdit)
+
+    const dispatch = useDispatch()
     const navigate = useNavigate()
-    const [edit, setEdit] = useState(false)
-    const [user, setUser] = useState({ firstName: data?.firstName, lastName: data?.lastName })
+
+    const handleEditButton = () => {
+        if (!isEditingUser) return dispatch(setEditingState())
+        const firstName = document.getElementById('firstName').value ? document.getElementById('firstName').value : data?.firstName
+        const lastName = document.getElementById('lastName').value ? document.getElementById('lastName').value : data?.lastName
+        dispatch(userEditor({ firstName, lastName }))
+    }
+
     useEffect(() => {
-        if (!isLogged) {
-            return navigate('/signIn')
-        }
+        if (!isLogged) return navigate('/signIn')
     }, [isLogged, navigate])
+
+
     return (
-        <main className="main bg-dark">
-            <div className="header">
-                <h1>Welcome back<br />
-                    {!edit && user.firstName + " " + user.lastName + "!"}
-                </h1>
-                {edit &&
-                    <div className='edit-user'>
-                        <input type="text" placeholder={user.firstName} id='firstName' />
-                        <input type="text" placeholder={user.lastName} id="lastName" />
-                    </div>}
-                <div className='edit-user'>
-                    <button className="edit-button" onClick={() => {
-                        if (!edit) return setEdit(true)
-                        const firstName = document.getElementById('firstName').value ? document.getElementById('firstName').value : user.firstName
-                        const lastName = document.getElementById('lastName').value ? document.getElementById('lastName').value : user.lastName
-                        console.log({ firstName, lastName })
-                        setUser({ firstName, lastName })
-                        setEdit(false)
-                    }}>{!edit ? "Edit Name" : "Save"}</button>
-                    {edit &&
-                        <button onClick={() => setEdit(false)} className='edit-button'>Cancel</button>
-                    }
-                </div>
-            </div>
-            {transactions.map((transaction, key) => (
-                <BankAccount key={key}
-                    title={transaction.title}
-                    amount={transaction.amount}
-                    balance={transaction.balance}
-                />
-            ))}
-        </main>
+        <>
+            {isLogged &&
+                <main className="main bg-dark">
+                    <div className="header">
+                        <h1>Welcome back<br />
+                            {!isEditingUser && data.firstName + " " + data.lastName + "!"}
+                        </h1>
+                        {isEditingUser &&
+                            <div className='edit-user'>
+                                <input type="text" placeholder={data.firstName} id='firstName' />
+                                <input type="text" placeholder={data.lastName} id="lastName" />
+                            </div>}
+                        <div className='edit-user'>
+                            <button className="edit-button" onClick={handleEditButton}>{!isEditingUser ? "Edit Name" : "Save"}</button>
+                            {isEditingUser &&
+                                <button onClick={() => dispatch(setEditingState())} className='edit-button'>Cancel</button>
+                            }
+                        </div>
+                        {error && <p className='display-error'>{error}</p>}
+                    </div>
+                    {transactions.map((transaction, key) => (
+                        <BankAccount key={key}
+                            title={transaction.title}
+                            amount={transaction.amount}
+                            balance={transaction.balance}
+                        />
+                    ))}
+                </main>
+            }
+        </>
     )
 }
