22a66a7d4b770937234bec835cd4f8530134adbd
omar <omar.tourabi7@gmail.com>
2023-05-23 12:05:43 +0200

    userdatamocked

---
+++ src/data/userDataMock.js
@@ -0,0 +1,20 @@
+export const user = {
+    "status": 200,
+    "message": "Successfully got user profile data",
+    "body": {
+        "email": "tony@stark.com",
+        "firstName": "Tony",
+        "lastName": "Stark",
+        "createdAt": "2023-05-19T10:19:14.971Z",
+        "updatedAt": "2023-05-19T10:19:14.971Z",
+        "id": "64674d226a91dbf95b973362"
+    }
+}
+
+/* {
+    "status": 200,
+    "message": "User successfully logged in",
+    "body": {
+        "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0Njc0ZDIyNmE5MWRiZjk1Yjk3MzM2MiIsImlhdCI6MTY4NDc4MDEwOCwiZXhwIjoxNjg0ODY2NTA4fQ.miSFyCy-YM5s1E6UsgaeV54Pt6Smu7VJbVqByZOCShU"
+    }
+} */
